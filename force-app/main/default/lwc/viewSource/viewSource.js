788a255917254908585f90305c7abd17e56ecbeb
Sgupt173 <SGupt173@its.jnj.com>
2022-05-17 17:56:21 +0530

    Fixing Pretirer

--- force-app/main/default/lwc/viewSource/viewSource.js
+++ force-app/main/default/lwc/viewSource/viewSource.js
@@ -3,7 +3,8 @@ import { LightningElement, api } from 'lwc';
 export default class ViewSource extends LightningElement {
     //baseURL = 'https://github.com/trailheadapps/lwc-recipes/tree/main/force-app/main/default/';
 
-    baseURL = 'https://github.com/Saddy108/LWC/tree/master/force-app/main/default';
+    baseURL =
+        'https://github.com/Saddy108/LWC/tree/master/force-app/main/default/';
 
     @api source;
 
