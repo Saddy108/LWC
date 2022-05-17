3e2fd00ca4c38694cf44f8465faf82a5dd09e3ff
Sgupt173 <SGupt173@its.jnj.com>
2022-05-17 18:45:11 +0530

    apiMethod

--- force-app/main/default/lwc/apiMethod/apiMethod.js
+++ force-app/main/default/lwc/apiMethod/apiMethod.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class ApiMethod extends LightningElement {
     handleRefresh() {
+        // SGN : Using querySelector we can call child components method
         this.template.querySelector('c-clock').refresh();
     }
 }
--- force-app/main/default/lwc/clock/clock.js
+++ force-app/main/default/lwc/clock/clock.js
@@ -3,6 +3,7 @@ import { LightningElement, api } from 'lwc';
 export default class Clock extends LightningElement {
     timestamp = new Date();
 
+    // SGN: If we need to call method then we can define as @api
     @api
     refresh() {
         this.timestamp = new Date();
