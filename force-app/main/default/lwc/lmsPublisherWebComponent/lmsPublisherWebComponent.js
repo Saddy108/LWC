4ef1d6386f38a818a081e6e57a6f63246642f1e5
Sgupt173 <SGupt173@its.jnj.com>
2022-05-17 19:17:13 +0530

    LmsPublisherWebComponent

--- force-app/main/default/lwc/lmsPublisherWebComponent/lmsPublisherWebComponent.js
+++ force-app/main/default/lwc/lmsPublisherWebComponent/lmsPublisherWebComponent.js
@@ -5,14 +5,19 @@ import getContactList from '@salesforce/apex/ContactController.getContactList';
 import { publish, MessageContext } from 'lightning/messageService';
 import RECORD_SELECTED_CHANNEL from '@salesforce/messageChannel/Record_Selected__c';
 
+// SGN : We need to import above all prebuilt methods, as well as LMS which we defined
+
 export default class LmsPublisherWebComponent extends LightningElement {
     @wire(getContactList)
     contacts;
 
+    // SGN : We need to call this wire function to initiate the MessageContext
     @wire(MessageContext)
     messageContext;
 
     // Respond to UI event by publishing message
+    // SGN : So whenever contact is clicked from child component, we are creating the payload and publishing it,using or passing
+    // 3 parameters
     handleContactSelect(event) {
         const payload = { recordId: event.target.contact.Id };
 
--- force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
+++ force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+// SGN : To retrieve the data we can use getRecord, And to copy the value or read the value we use getFieldValue
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { reduceErrors } from 'c/ldsUtils';
@@ -6,6 +7,7 @@ import { reduceErrors } from 'c/ldsUtils';
 // Import message service features required for subscribing and the message channel
 import { subscribe, MessageContext } from 'lightning/messageService';
 import RECORD_SELECTED_CHANNEL from '@salesforce/messageChannel/Record_Selected__c';
+// SGN : Above all prebuilt methods and also messageservice are need to be imported
 
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import TITLE_FIELD from '@salesforce/schema/Contact.Title';
@@ -36,6 +38,7 @@ export default class LmsSubscriberWebComponent extends LightningElement {
         if (error) {
             this.dispatchToast(error);
         } else if (data) {
+            // SGN : This is also a way by which we can read the data and assign value to individual variable
             fields.forEach(
                 (item) => (this[item.fieldApiName] = getFieldValue(data, item))
             );
@@ -44,10 +47,12 @@ export default class LmsSubscriberWebComponent extends LightningElement {
 
     // By using the MessageContext @wire adapter, unsubscribe will be called
     // implicitly during the component descruction lifecycle.
+    // SGN : We always need to call the MessageContext using wire to initiate
     @wire(MessageContext)
     messageContext;
 
     // Encapsulate logic for LMS subscribe.
+    // SGN : This is how we subscribe the LMS, and we can subscribe this in connectedcallback
     subscribeToMessageChannel() {
         this.subscription = subscribe(
             this.messageContext,
@@ -62,6 +67,7 @@ export default class LmsSubscriberWebComponent extends LightningElement {
     }
 
     // Standard lifecycle hooks used to sub/unsub to message channel
+    // SGN : In the lifecycle hook we can subscribe it
     connectedCallback() {
         this.subscribeToMessageChannel();
     }
