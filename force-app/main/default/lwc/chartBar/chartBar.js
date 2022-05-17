c3eba201754e98a624aca542f4b95ed9723e4b62
Sgupt173 <SGupt173@its.jnj.com>
2022-05-17 18:36:45 +0530

    apiProperty

--- force-app/main/default/lwc/chartBar/chartBar.js
+++ force-app/main/default/lwc/chartBar/chartBar.js
@@ -2,7 +2,9 @@ import { LightningElement, api } from 'lwc';
 
 export default class ChartBar extends LightningElement {
     @api percentage;
+    // SGN : Used API beside property to make it public and also to receive from parent
 
+    // SGN : User backtick to generate the width of the style
     get style() {
         return `width: ${this.percentage}%`;
     }
